92ce32058a4f3e48a1c04b6551ab35f48521725c
Jakub Matejik <matejik@ttc.cz>
2023-05-09 00:25:48 +0200

    aa

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+const { off } = require("process");
+
+module.exports = {
+  root: true,
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:vue/essential',
+    '@vue/standard',
+    '@vue/typescript/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    "quotes": "off",
+    "semi": "off",
+    "quote-props": "off",
+    "comma-dangle": "off",
+    "space-before-function-paren": "off"
+  }
+}
